5330c178ebf19f1fa7c9e3665b7c80ddf5eda7e0
yohom <382146139@qq.com>
2019-12-01 21:00:51 +0800

    fix: [objc] 每个.h文件都import <Flutter/Flutter.h>

--- ios/Classes/CGPointHandler.h
+++ ios/Classes/CGPointHandler.h
@@ -3,8 +3,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Flutter/Flutter.h>
 
+NS_ASSUME_NONNULL_BEGIN
 
 @interface CGPointHandler : NSObject
 - (instancetype)initWith:(NSString*) method :(NSDictionary*)args :(FlutterResult)methodResult;
 @end
+
+NS_ASSUME_NONNULL_END
--- ios/Classes/FoundationFluttifyPlugin.h
+++ ios/Classes/FoundationFluttifyPlugin.h
@@ -1,5 +1,9 @@
 #import <Flutter/Flutter.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface FoundationFluttifyPlugin : NSObject<FlutterPlugin>
 - (instancetype) initWithRegistrar:(NSObject<FlutterPluginRegistrar>*) registrar;
 @end
+
+NS_ASSUME_NONNULL_END
