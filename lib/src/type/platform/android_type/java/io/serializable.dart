85544422040296e98513f36d175c10e36377e406
yohom <yohombao@gmail.com>
2019-12-29 21:23:28 +0800

    fix: java_io_Serializable应该为mixin

--- lib/src/type/platform/android_type/java/io/serializable.dart
+++ lib/src/type/platform/android_type/java/io/serializable.dart
@@ -1,4 +1,4 @@
 import 'package:foundation_fluttify/src/type/base/ref.dart';
 
 // ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
-class java_io_Serializable extends Ref {}
+mixin java_io_Serializable on Ref {}
