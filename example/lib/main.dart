8de56954aab85240bea549bc757c556c138efd48
yohom <yohombao@gmail.com>
2020-01-01 20:19:16 +0800

    enhance: 调整example的ui

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:foundation_fluttify/foundation_fluttify.dart';
 
@@ -13,22 +14,20 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: RaisedButton(
-            onPressed: () async {
-              await _test();
-            },
-            child: Text('Running on'),
-          ),
+        appBar: AppBar(title: const Text('Plugin example app')),
+        body: DecoratedColumn(
+          children: <Widget>[
+            RaisedButton(
+              onPressed: _testAddProperty,
+              child: Text('AddProperty'),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  Future<void> _test() async {
+  Future<void> _testAddProperty() async {
     final target = await CGPoint.create(225, 556);
     final target1 = await CGPoint.create(112, 556);
     await target.addProperty(1, target1);
