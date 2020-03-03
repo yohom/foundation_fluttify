486d62d178aa52ef0df741c71848b6860a12f3f9
yohom <yohombao@qq.com>
2020-03-03 12:46:25 +0800

    fix: CLLocationCoordinate2DHandler方法分发错误

--- ios/Classes/CLLocationCoordinate2DHandler.m
+++ ios/Classes/CLLocationCoordinate2DHandler.m
@@ -31,6 +31,37 @@ void CLLocationCoordinate2DHandler(NSString* method, NSDictionary* args, Flutter
         [dataValue getValue:&_structValue];
         
         methodResult(@(_structValue.longitude));
+    }
+    // 创建CLLocationCoordinate2D
+    else if ([@"CLLocationCoordinate2D::createCLLocationCoordinate2D" isEqualToString:method]) {
+        CLLocationDegrees latitude = [args[@"latitude"] doubleValue];
+        CLLocationDegrees longitude = [args[@"longitude"] doubleValue];
+        
+        CLLocationCoordinate2D data = CLLocationCoordinate2DMake(latitude, longitude);
+        
+        NSValue *dataValue = [NSValue value:&data withObjCType:@encode(CLLocationCoordinate2D)];
+        HEAP[@(dataValue.hash)] = dataValue;
+        
+        methodResult(@(dataValue.hash));
+    }
+    // 批量创建CLLocationCoordinate2D
+    else if ([@"CLLocationCoordinate2D::create_batchCLLocationCoordinate2D" isEqualToString:method]) {
+        NSArray<NSNumber*>* latitudeBatch = (NSArray<NSNumber*>*) args[@"latitude_batch"];
+        NSArray<NSNumber*>* longitudeBatch = (NSArray<NSNumber*>*) args[@"longitude_batch"];
+        
+        NSMutableArray<NSNumber*>* resultBatch = [NSMutableArray arrayWithCapacity:latitudeBatch.count];
+        for (int i = 0; i < latitudeBatch.count; i++) {
+            CLLocationDegrees latitude = [[latitudeBatch objectAtIndex:i] doubleValue];
+            CLLocationDegrees longitude = [[longitudeBatch objectAtIndex:i] doubleValue];
+            
+            CLLocationCoordinate2D data = CLLocationCoordinate2DMake(latitude, longitude);
+
+            NSValue *dataValue = [NSValue value:&data withObjCType:@encode(CLLocationCoordinate2D)];
+            [resultBatch addObject:@(dataValue.hash)];
+            HEAP[@(dataValue.hash)] = dataValue;
+        }
+        
+        methodResult(resultBatch);
     } else {
         methodResult(FlutterMethodNotImplemented);
     }
