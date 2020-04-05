76f4e1807731d444d43ea7f71dc78bdcba50e8dd
yohom <yohombao@qq.com>
2020-04-05 09:59:12 +0800

    feat: NSError增加获取code和description的方法

--- lib/src/type/platform/ios_type/ns_error.dart
+++ lib/src/type/platform/ios_type/ns_error.dart
@@ -1,3 +1,17 @@
+import 'package:foundation_fluttify/foundation_fluttify.dart';
+
 import 'ns_object.dart';
 
-class NSError extends NSObject {}
+class NSError extends NSObject {
+  Future<int> get code async {
+    final int errorCode =
+        await kMethodChannel.invokeMethod('NSError::getCode', {'refId': refId});
+    return errorCode;
+  }
+
+  Future<String> get description async {
+    final String description = await kMethodChannel
+        .invokeMethod('NSError::getDescription', {'refId': refId});
+    return description;
+  }
+}
