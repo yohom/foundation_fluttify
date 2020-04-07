3d861b7c069f19afac914b0f0ce642511ba541ca
yohom <yohombao@qq.com>
2020-04-07 08:42:42 +0800

    enhance: [breaking change] Ref类内的方法和属性都加上__后缀, 防止和子类冲突

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -54,9 +54,9 @@ class _MyAppState extends State<MyApp> {
   Future<void> _testAddProperty() async {
     final target = await CGPoint.create(225, 556);
     final target1 = await CGPoint.create(112, 556);
-    await target.addProperty(1, target1);
+    await target.addProperty__(1, target1);
 
-    final result = await target.getProperty(1);
+    final result = await target.getProperty__(1);
     final target1Result = CGPoint()..refId = result.refId;
     debugPrint('结果: ${await target1Result.x}');
   }
