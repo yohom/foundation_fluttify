5c8d52d28b94c6c0ce0b55343b43696705250070
yohom <382146139@qq.com>
2020-06-22 18:22:58 +0800

    enhance: CGSize增加width和height

--- lib/src/type/platform/ios_type/cg_size.dart
+++ lib/src/type/platform/ios_type/cg_size.dart
@@ -1,3 +1,11 @@
 import 'package:foundation_fluttify/foundation_fluttify.dart';
 
-class CGSize extends Ref {}
+class CGSize extends Ref {
+  Future<double> get width {
+    return kMethodChannel.invokeMethod('CGSize::getWidth', {'refId': refId});
+  }
+
+  Future<double> get height {
+    return kMethodChannel.invokeMethod('CGSize::getHeight', {'refId': refId});
+  }
+}
