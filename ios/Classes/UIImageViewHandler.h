b4529a38c2e86c74660f64b2f6c5a17dfede97ce
yohom <382146139@qq.com>
2020-07-02 08:29:36 +0800

    feat: 增加UIImageView的创建

---
+++ ios/Classes/UIImageViewHandler.h
@@ -0,0 +1,15 @@
+//
+//  UIImageViewHandler.h
+//  foundation_fluttify
+//
+//  Created by Yohom Bao on 2020/7/2.
+//
+
+#import <Foundation/Foundation.h>
+#import <Flutter/Flutter.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+void UIImageViewHandler(NSString* method, id args, FlutterResult methodResult);
+
+NS_ASSUME_NONNULL_END
