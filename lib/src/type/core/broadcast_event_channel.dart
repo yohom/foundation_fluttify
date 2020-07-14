789fdf2cd21388bd0afa93ad77a3bd97254155ef
yohom <382146139@qq.com>
2020-07-14 11:13:58 +0800

    enhance: 新增FluttifyBroadcastEventChannel, 一律返回Intent

---
+++ lib/src/type/core/broadcast_event_channel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/services.dart';
+import 'package:foundation_fluttify/src/type/platform/android_type/android/content/intent.dart';
+
+class FluttifyBroadcastEventChannel extends EventChannel {
+  const FluttifyBroadcastEventChannel(String name) : super(name);
+
+  Stream<android_content_Intent> receiveBroadcastStream([dynamic arguments]) {
+    return super
+        .receiveBroadcastStream(arguments)
+        .map((event) => android_content_Intent()..refId = event);
+  }
+}
