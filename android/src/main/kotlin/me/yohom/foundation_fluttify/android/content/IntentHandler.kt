ce046a1f8924c0aa91b4239961986c0d2e099bce
yohom <382146139@qq.com>
2020-07-14 11:43:11 +0800

    enhance: Intent增加获取bundle

--- android/src/main/kotlin/me/yohom/foundation_fluttify/android/content/IntentHandler.kt
+++ android/src/main/kotlin/me/yohom/foundation_fluttify/android/content/IntentHandler.kt
@@ -1,9 +1,26 @@
 package me.yohom.foundation_fluttify.android.content
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.plugin.common.MethodChannel
+import me.yohom.foundation_fluttify.HEAP
 
 fun IntentHandler(method: String, args: Any, methodResult: MethodChannel.Result) {
     when (method) {
+        "android.content.Intent::getBundle" -> {
+            val refId = (args as Map<String, Any>)["refId"] as Int
+            val intent = HEAP[refId] as Intent
+
+            val bundle = intent.extras?: Bundle.EMPTY
+
+            // bundle转为map 暂时不考虑非jsonable的类型, 一律认为可以直接传递
+            val result: MutableMap<String, java.io.Serializable?> = mutableMapOf()
+            for (key in bundle.keySet()) {
+                result[key] = bundle.getSerializable(key)
+            }
+
+            methodResult.success(result)
+        }
         else -> methodResult.notImplemented()
     }
 }
--- lib/src/type/platform/android_type/android/content/intent.dart
+++ lib/src/type/platform/android_type/android/content/intent.dart
@@ -1,4 +1,12 @@
+import 'package:foundation_fluttify/src/object/obejcts.dart';
+import 'package:foundation_fluttify/src/type/platform/android_type/android/os/bundle.dart';
 import 'package:foundation_fluttify/src/type/platform/android_type/java/lang/object.dart';
 
 // ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
-class android_content_Intent extends java_lang_Object {}
+class android_content_Intent extends java_lang_Object {
+  Future<Map<String, dynamic>> get bundle async {
+    final result = await kMethodChannel.invokeMapMethod<String, dynamic>(
+        'android.content.Intent::getBundle', {'refId': refId});
+    return result;
+  }
+}
