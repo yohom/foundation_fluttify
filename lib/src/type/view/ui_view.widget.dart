878d9061cd8cbc0303f8360e3088749c4c3b2792
yohom <382146139@qq.com>
2020-07-23 14:51:23 +0800

    fix: UIViewWidget释放

--- lib/src/type/view/ui_view.widget.dart
+++ lib/src/type/view/ui_view.widget.dart
@@ -4,14 +4,24 @@ import 'package:flutter/material.dart';
 import 'package:foundation_fluttify/src/type/platform/ios_type/ui_view.dart';
 
 typedef Future<void> OnUIViewCreated(UIView controller);
+typedef Future<void> _OnUiKitViewDispose();
 
-class UIViewWidget extends StatelessWidget {
+class UIViewWidget extends StatefulWidget {
   const UIViewWidget({
     Key key,
     this.onUIViewCreated,
+    this.onDispose,
   }) : super(key: key);
 
   final OnUIViewCreated onUIViewCreated;
+  final _OnUiKitViewDispose onDispose;
+
+  @override
+  _UIViewWidgetState createState() => _UIViewWidgetState();
+}
+
+class _UIViewWidgetState extends State<UIViewWidget> {
+  UIView _view;
 
   @override
   Widget build(BuildContext context) {
@@ -19,17 +29,26 @@ class UIViewWidget extends StatelessWidget {
       return UiKitView(
         viewType: 'me.yohom/foundation_fluttify/UIView',
         onPlatformViewCreated: (viewId) {
-          if (onUIViewCreated != null) {
-            onUIViewCreated(
-              UIView()
-                ..refId = 2147483647 - viewId
-                ..tag__ = 'platform',
-            );
+          _view = UIView()
+            ..refId = 2147483647 - viewId
+            ..tag__ = 'platform';
+          if (widget.onUIViewCreated != null) {
+            widget.onUIViewCreated(_view);
           }
         },
       );
     } else {
       return Center(child: Text('不支持的平台'));
     }
+  }
+
+  @override
+  void dispose() {
+    if (widget.onDispose != null) {
+      widget.onDispose().then((_) => _view.release__());
+    } else {
+      _view.release__();
+    }
+    super.dispose();
   }
 }
