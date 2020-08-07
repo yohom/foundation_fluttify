87ca9ac22b937186d729a0fe52387da5d92687da
yohom <382146139@qq.com>
2020-08-07 14:56:13 +0800

    fix: ImageProvider转图片数据

--- lib/src/extension/image_provider.x.dart
+++ lib/src/extension/image_provider.x.dart
@@ -14,15 +14,17 @@ extension ImageProviderX on ImageProvider {
       debugPrint('命中缓存');
       completer.complete(_cache[key.toString()]);
     } else {
-      this
-          .resolve(config)
-          .addListener(ImageStreamListener((imageInfo, sync) async {
+      ImageStreamListener listener;
+      ImageStream stream = this.resolve(config); //获取图片流
+      listener = ImageStreamListener((imageInfo, sync) async {
         final byteData =
             await imageInfo.image.toByteData(format: ImageByteFormat.png);
         final result = byteData.buffer.asUint8List();
         _cache[key.toString()] = result;
         completer.complete(result);
-      }));
+        stream.removeListener(listener); //移除监听
+      });
+      stream.addListener(listener);
     }
 
     return completer.future;
