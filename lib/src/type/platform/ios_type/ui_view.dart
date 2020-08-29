50efcb3f7fd5425ae5ce0cd2bb4de048d390cc15
yohom <382146139@qq.com>
2020-08-29 12:57:02 +0800

    enhance: 优化scaleWithDuration

--- lib/src/type/platform/ios_type/ui_view.dart
+++ lib/src/type/platform/ios_type/ui_view.dart
@@ -19,25 +19,23 @@ class UIView extends NSObject {
 
   Future<void> scaleWithDuration({
     double duration = 1,
-    double x = 0,
-    double toX = 1,
-    double y = 0,
-    double toY = 1,
+    double fromValue = 0,
+    double toValue = 1,
+    int repeatCount = 0,
   }) async {
     assert(duration != null);
     await kMethodChannel.invokeMethod('UIView::scaleWithDuration', {
       'refId': refId,
       'duration': duration,
-      'x': x,
-      'toX': toX,
-      'y': y,
-      'toY': toY,
+      'fromValue': fromValue,
+      'toValue': toValue,
+      'repeatCount': repeatCount,
     });
   }
 
   Future<CGRect> get frame async {
     final result =
-        await kMethodChannel.invokeMethod('UIView::getFrame', {'refId': refId});
+    await kMethodChannel.invokeMethod('UIView::getFrame', {'refId': refId});
     return CGRect()..refId = result;
   }
 
