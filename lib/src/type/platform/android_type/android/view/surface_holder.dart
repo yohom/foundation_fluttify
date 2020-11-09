875dc7ae36442edf3dca0e9607d9ff6ffea778eb
yohom <382146139@qq.com>
2020-11-09 20:04:24 +0800

    enhance: 增加android_view_SurfaceHolder

---
+++ lib/src/type/platform/android_type/android/view/surface_holder.dart
@@ -0,0 +1,17 @@
+import 'package:foundation_fluttify/foundation_fluttify.dart';
+
+class _android_view_SurfaceHolder_SUB extends java_lang_Object
+    with android_view_SurfaceHolder {}
+
+mixin android_view_SurfaceHolder on java_lang_Object {
+  static android_view_SurfaceHolder subInstance() =>
+      _android_view_SurfaceHolder_SUB();
+
+  @override
+  final String tag__ = 'platform';
+}
+
+mixin android_view_SurfaceHolder_Callback on java_lang_Object {
+  @override
+  final String tag__ = 'platform';
+}
