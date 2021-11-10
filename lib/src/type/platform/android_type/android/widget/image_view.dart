6502e1a2f16d8bde8104dd42203041c62a0001b1
yohom <yohombao@qq.com>
2021-11-10 10:29:59 +0800

    enhance: 增加ImageViewHandler

--- lib/src/type/platform/android_type/android/widget/image_view.dart
+++ lib/src/type/platform/android_type/android/widget/image_view.dart
@@ -1,7 +1,18 @@
 // ignore_for_file: non_constant_identifier_names, camel_case_types, missing_return, unused_import
+import 'package:foundation_fluttify/foundation_fluttify.dart';
 import 'package:foundation_fluttify/src/type/platform/android_type/android/view/view.dart';
 
 class android_widget_ImageView extends android_view_View {
   @override
   final String tag__ = 'platform';
+
+  Future<android_widget_ImageView> createWithBitmap(
+    android_graphics_Bitmap bitmap,
+  ) async {
+    final result = await kMethodChannel.invokeMethod<Ref>(
+      'android.widget.ImageView::createWithBitmap',
+      {'bitmap': bitmap},
+    );
+    return android_widget_ImageView()..refId = result?.refId;
+  }
 }
